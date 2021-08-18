e2086ba332133d59d7cd6e35920e1f4f2012f5b3
Kay <kauuka@gmail.com>
2021-08-18 01:54:12 -0700

    worked on fragment layouts

--- app/src/main/java/com/example/recipe_nutrition/MainActivity.kt
+++ app/src/main/java/com/example/recipe_nutrition/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.recipe_nutrition
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //reference of navController for the onSupport function
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //setting up the nav views to switch between fragments
+        val navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.findRecipesFragment, R.id.myRecipesFragment, R.id.nutritionFragment))
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+    }
+
+    //switches between fragments in the main menu
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
